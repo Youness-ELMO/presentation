0c29fb448cb4b91e77aff3f6afa22aa33c197951
Youness <Youness@DESKTOP-A7ECOHN>
2022-03-07 22:11:08 +0100

    Ajoutez des fichiers projet.

---
+++ pjt class/Personne.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjt_class
+{
+    public class Personne
+    {
+        public Personne(string n, string p, int a, string v)
+        {
+            nom = n;
+            prenom = p;
+            age = a;
+            ville = v;
+        }
+        public Personne() { }//constructeur par defeau
+
+        public string nom;
+        public string prenom;
+        public int age;
+        public string ville;
+
+        public String Affichage1()
+        {
+            String s = "   Je m’appelle    " + nom + "      " + prenom + "   J’ai  " + age + " ans   J’habite à   " + ville;
+            return s;
+
+        }
+        public void Affichage2()
+        {
+            String s = "   Je m’appelle    " + nom + "      " + prenom + "   J’ai  " + age + " ans   J’habite à   " + ville;
+            Console.WriteLine(s);
+           
+        }
+    }
+}
---
+++ pjt class/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pjt_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne();
+            p1.nom = "Yuouness";
+            p1.prenom = "EL MODAHHAB";
+            p1.age = 22;
+            p1.ville = "casa";
+
+            Personne p2 = new Personne();
+            Console.Write("donner le nom: ");
+            p2.nom = Console.ReadLine();
+            Console.Write("donner le prenom: ");
+            p2.prenom = Console.ReadLine();
+            Console.Write("donner l'age: ");
+            p2.age = int.Parse(Console.ReadLine());
+            Console.Write("donner la ville: ");
+            p2.ville = Console.ReadLine();
+
+            Console.WriteLine(p1.Affichage1());
+            
+            p2.Affichage2();
+
+
+            Personne p3 = new Personne("YNS","ELMO",30, "fes");
+            p3.Affichage2();
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
